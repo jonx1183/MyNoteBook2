0709679b086881e9d802921e1c29844f646de001
jonx1183 <jonx1183@stud.kea.dk>
2023-09-04 14:00:19 +0200

    Added list

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Button, View, TextInput } from 'react-native';
+import { StyleSheet, FlatList, Button, View, TextInput, Text } from 'react-native';
 
 
 
 export default function App() {
   
   const[text, setText] = useState('')
+  const notes = [{key:1, name:"Jon"}, {key:2, name:"Testington"}]
 
   function buttenHandler(){
     alert("You typed: " + text)
     }
   return (
     <View style={styles.container}>
-      <TextInput onChangeText={(txt) => setText(txt)} />
+      <TextInput style={styles.TextInput} onChangeText={(txt) => setText(txt)} />
       <button title='pressMe' onClick={buttenHandler}>Press</button>
+      <FlatList 
+       data={notes}
+       renderItem={(note) => <Text>{note.item.name}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -26,5 +31,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 120
   },
+  TextInput:{
+    backgroundColor: 'lightblue',
+    minWidth: 200
+  }
 });
